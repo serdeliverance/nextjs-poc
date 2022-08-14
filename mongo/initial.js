6f8b2104507403c4ce1bb1abd50745eaa41ef58a
Sergio Cano <canosergio90@gmail.com>
2022-08-15 01:23:06 +0200

    pre rendering meetups in main page and load meetups on mongo db
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

---
+++ mongo/initial.js
@@ -0,0 +1,16 @@
+db.meetup.insertMany([
+  {
+    id: 'm1',
+    title: 'A first meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/540px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, Some city',
+    description: 'This is a first meetup'
+  },
+  {
+    id: 'm2',
+    title: 'Hunter x Hunter',
+    image: 'https://media.gq-magazin.de/photos/628e35d2be670efaab242579/16:9/w_2560%2Cc_limit/HunterxHunter-Aufmacher.jpg',
+    address: 'Some address 5, Some city',
+    description: 'Hunter x Hunter fans'
+  }
+])
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,5 @@
+import { MongoClient } from 'mongodb'
 import MeetupList from '../components/meetups/MeetupList'
-
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A first meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/540px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 5, Some city',
-    description: 'This is a first meetup'
-  },
-  {
-    id: 'm2',
-    title: 'A second meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/540px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 5, Some city',
-    description: 'This is a second meetup'
-  }
-]
 
 function HomePage (props) {
   return <MeetupList meetups={props.meetups} />
@@ -39,9 +23,23 @@ function HomePage (props) {
 
 export async function getStaticProps () {
   // fetch data from API
+  const client = await MongoClient.connect(process.env.DB_URL)
+  const db = client.db()
+
+  const meetupCollection = db.collection('meetup')
+
+  const meetups = await meetupCollection.find().toArray()
+
+  client.close()
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS
+      meetups: meetups.map(meetup => ({
+        title: meetup.title,
+        image: meetup.image,
+        address: meetup.address,
+        id: meetup.id
+      }))
     }
   }
 }
